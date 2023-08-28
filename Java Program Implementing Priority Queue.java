540bab1e7dc8a049396585ab4c348ab2a4fd3b76
Tanisha Srivastava <138584422+tanishasrivastava@users.noreply.github.com>
2023-08-28 12:39:13 +0530

    Create Java Program Implementing Priority Queue.java

---
+++ Java Program Implementing Priority Queue.java
@@ -0,0 +1,26 @@
+import java.util.*;  
+public class Queue {
+public static void main(String args[]){  
+PriorityQueue<String> queue=new PriorityQueue<String>();  
+queue.add("A");  
+queue.add("B");  
+queue.add("C");  
+queue.add("D");  
+queue.add("E");  
+
+System.out.println("head:"+queue.element());  
+System.out.println("head:"+queue.peek());  
+System.out.println("iterating the queue elements:");  
+Iterator itr=queue.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+queue.remove();  
+queue.poll();  
+System.out.println("after removing two elements:");  
+Iterator<String> itr2=queue.iterator();  
+while(itr2.hasNext()){  
+System.out.println(itr2.next());  
+}  
+}  
+}
